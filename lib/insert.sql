55ee00774ca1367060642f8a724654b80318b9aa
Marlon Merjos <marlonmerjos@gmail.com>
2017-08-14 13:28:15 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+  ("Mr. Chocolate", 10, "M", "black","kind",0),
+  ("Rowdy", 10, "M", "black","kind",1),
+  ("Tabitha", 10, "F", "black","kind",1),
+  ("Sergeant Brown", 10, "M", "black","kind",1),
+  ("Melissa", 10, "F", "black","mean",0),
+  ("Grinch", 10, "F", "black","kind",1),
+  ("Wendy", 10, "F", "black","kind",1),
+  (null, 10, "M", "black","mean",0);
